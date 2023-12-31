adfd3197456f11e917b7f636b678e3a8a6897ceb
Pabloseedem <pavega@seedem.co>
2023-12-31 11:50:25 -0500

    Create components with content.

---
+++ src/componentes/testimonios.js
@@ -0,0 +1,20 @@
+// import '../hojas-de-estilo/Testimonio.css';
+//Componente funcional
+function Testimonio(props) {
+    return (
+        <div className='contenedor-testimonio'>
+            <img
+                className='imagen-testimonio'
+                src={require(`../imagenes/${props.imagen}-freecodecamp.png`)}
+                alt='Foto de Emma' />
+
+            <div className='contenedor-texto-testimonio'>
+                <p className='nombre-testimonio'>  <strong>{props.nombre}</strong> en {props.pais}</p>
+                <p className='cargo-testimonio'> {props.cargo}en <strong>{props.empresa}</strong></p>
+                <p className='texto-testimonio'> "{props.testimonio}"</p>
+            </div>
+        </div>
+    );  
+}
+//por defecto significa que este va a ser el unico archivo que se va a exportar
+export default Testimonio;

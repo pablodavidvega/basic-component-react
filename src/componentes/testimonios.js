1c12204ba7fb503ffc5e3b21d4224a8819770c76
Pabloseedem <pavega@seedem>
2024-03-10 16:30:05 -0500

    Creacion componentes

--- src/componentes/testimonios.js
+++ src/componentes/testimonios.js
@@ -1,20 +1,28 @@
-import './hojas-de-estilo/Testimonio.css';
+import "./hojas-de-estilo/Testimonio.css";
 //Componente funcional
 function Testimonio(props) {
-    return (
-        <div className='contenedor-testimonio'>
-            <img
-                className='imagen-testimonio'
-                src={require(`../imagenes/${props.imagen}-freecodecamp.png`)}
-                alt='Foto de Emma' />
+  return (
+    <div className="contenedor-testimonio">
+      <img
+        className="imagen-testimonio"
+        src={require(`../imagenes/${props.imagen}-freecodecamp.png`)}
+        alt={`Foto de ${props.alt}`}
+      />
 
-            <div className='contenedor-texto-testimonio'>
-                <p className='nombre-testimonio'>  <strong>{props.nombre}</strong> en {props.pais}</p>
-                <p className='cargo-testimonio'> {props.cargo}en <strong>{props.empresa}</strong></p>
-                <p className='texto-testimonio'> "{props.testimonio}"</p>
-            </div>
-        </div>
-    );  
+      <div className="contenedor-texto-testimonio">
+        <p className="nombre-testimonio">
+          {" "}
+          <strong>{props.nombre}</strong> en {props.pais}
+        </p>
+        <p className="cargo-testimonio">
+          {" "}
+          {props.cargo}en <strong>{props.empresa}</strong>
+        </p>
+        <p className="texto-testimonio"> {props.testimonio}</p>
+        <a className="emailTestimonios">{props.email}</a>
+      </div>
+    </div>
+  );
 }
 //por defecto significa que este va a ser el unico archivo que se va a exportar
 export default Testimonio;
